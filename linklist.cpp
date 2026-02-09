21594d0f9b0a5c88766a9a8f58fedbc3ae6d3cf9
Ruksna khatun <iitianruksana@gmail. com>
2026-02-09 16:50:54 +0530

    first commit

---
+++ linklist.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout<<"Hello, World!" << endl;
+    cout<<"Hello, World!" << endl;
+    cout<<"Ruksna khatun"<<endl;
+    for(int i=0;i<10;i++){
+        cout<<i<<endl;
+    }
+    for(int i=0;i<=20;i++){
+        cout<<"RUKSNA KHATUN"<<endl;
+    }
+    for(int i=1;i<10;i++){
+        cout<<"ramsha sultan"<<endl;
+        cout<<"Linklist file is running"<<endl;
+    }
+    cout<<"Linklist file is running"<<endl;
+  int array[i]=[1 2 3 4 5];
+  for(int i=0;i<5;i++){
+        cout<<array[i]<<endl;
+    }
+    return 0;
+}
