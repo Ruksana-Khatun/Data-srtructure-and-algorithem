fc4bca7a0b2257d4330c3fa0df6d3adbdec89234
Ruksna khatun <iitianruksana@gmail. com>
2026-02-09 17:12:40 +0530

    output input variable code done

--- linklist.cpp
+++ linklist.cpp
@@ -16,9 +16,39 @@ int main() {
         cout<<"Linklist file is running"<<endl;
     }
     cout<<"Linklist file is running"<<endl;
-  int array[i]=[1 2 3 4 5];
+  int array[5]={1,2,3,4,5};
   for(int i=0;i<5;i++){
         cout<<array[i]<<endl;
+    }
+cout<<"this is the end of the for loop"<<endl;
+
+    int a;
+    cout<<"enter your number"<<endl;
+   cin>>a;
+   int b;
+    cout<<"Enter your number"<<endl;
+    cin>>b;
+    cout<<"sum of a and b is "<<a+b<<endl;
+   int c;
+   cout<<"Enter your number"<<endl;
+   cin>>c;
+   int d;
+    cout<<"Enter your number"<<endl;
+    cin>>d; 
+    cout<<"ADD FLOAT NUMBERS= "<<c+d<<endl;
+    char e;
+    cout<<"Enter your number"<<endl;
+    cin>>e;
+
+    char f;
+    cout<<"Enter your number"<<endl;
+    cin>>f;
+    cout<<"ading char "<<e+f<<endl;
+    if (e==f){
+        cout<<"both char are same"<<endl;
+    }
+    else{
+        cout<<"both char are different"<<endl;
     }
     return 0;
 }
