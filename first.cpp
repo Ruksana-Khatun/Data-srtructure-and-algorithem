e96f835e7ec16d4ad004721103fdef68ebeb1f40
Ruksna khatun <iitianruksana@gmail.com>
2026-06-29 16:06:09 +0530

    setup done

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout<<"HI I have besn setup ths env file folde many timse but i hadd ben faliing multiple times  finalyy today i didi it"<<endl;
+    return 0;
+}
