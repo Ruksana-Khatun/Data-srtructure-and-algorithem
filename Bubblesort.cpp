dfbc51a6b7c03a80ecea51cd17a5ef9b18223e9b
Ruksna khatun <iitianruksana@gmail.com>
2026-07-15 12:35:26 +0530

    added more dsa

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
-void bublesort(int arr[],int n){
-   
-    
 
-}
 int main(){
     int arr[]={5,4,1,3,2};
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+             
+            }
             
         }
     }
-    bublesort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
+    
+    
 
 }
---
+++ busell.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+void buysell(int price[],int n){
+   
+
+    ///I JUST LEAVE THIS QUESTION CAUSE I DID NOT UNDERSTAND THIS ONE
+
+
+
+
+
+}
+int main(){
+    int price[]={7,1,5,3,6,4};
+    int n=6;
+
+
+}
